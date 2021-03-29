64dd45f8ef86f0c38556a56fb4cf7e93bb1c3e28
dalijp147 <medali.bouderbala@esprit.tn>
2021-03-29 16:04:10 +0200

    crud +metier

---
+++ gestion_client/clicom.h
@@ -0,0 +1,56 @@
+#ifndef CLICOM_H
+#define CLICOM_H
+
+#include <QDialog>
+#include "client.h"
+#include "commande.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class Clicom;
+}
+QT_END_NAMESPACE
+class Clicom : public QDialog
+{
+    Q_OBJECT
+
+public:
+    explicit Clicom(QWidget *parent = nullptr);
+    ~Clicom();
+
+private slots:
+    void on_Ajouter_clicked();
+
+    void on_supprimer_3_clicked();
+
+    void on_pb_ajouter_clicked();
+
+    void on_pb_supprimer_clicked();
+
+   //  void on_push_recherche_clicked();
+
+
+    //void on_combo_activated();
+
+    //void on_combo_activated(const QString &arg1);
+
+    void on_trie_activated();
+
+    void on_modifier_clicked();
+    //comment
+
+    void on_triecommande_activated();
+
+    void on_modifiercom_clicked();
+
+    void on_push_recherche_3_clicked();
+
+    void on_rech_but_clicked();
+
+private:
+    Ui::Clicom *ui;
+    Client C;
+    Commande F;
+};
+
+#endif // CLICOM_H
